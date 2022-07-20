7da6ba541025dbb0d8e7423b1ac045adb13c037e
Jimmy Chan <jchan85@gatech.edu>
2022-07-20 01:12:50 -0400

    Cleaned up some more code

--- node-express-typescript/dist/index.js
+++ node-express-typescript/dist/index.js
@@ -4,16 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-//import dotenv from 'dotenv';
-// import UserInfo from "./model/UserInfo";
 const db = require('./db_conn/db_conn');
 const port = process.env.PORT;
-// app.get('/', (req: Request, res: Response) => {
-//     res.send('Express + TypeScript Server is running');
-// });
 db.once('open', () => {
     app_1.default.listen(port, () => {
         console.log(`[server]: Server is running at http://localhost:${port}`);
     });
 });
-// const user = UserInfo;
